17d4fda4cec0d5c646d2a906b5cd25700e0bd094
Bruno Simioni <brunosimioni@gmail.com>
2017-03-31 22:47:36 -0300

    Initial tests with react navigation

---
+++ Streamline/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Linking,
+  Button,
+  View,
+} from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
+
+const AppNavigator = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+});
+
+export default () => <AppNavigator />;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
